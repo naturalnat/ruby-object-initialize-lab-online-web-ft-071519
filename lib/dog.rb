f707eddf4d3b4c60c644804cec7c453ae0413f00
Natalie Taktachev <ntaktach@gmail.com>
2019-07-22 19:48:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,15 +11,15 @@ class Dog
     @name
   end
 
-  def initialize(breed)
-    @breed = breed
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
+  # def initialize(breed)
+  #   @breed = breed
+  # end
+  # 
+  # def breed=(breed)
+  #   @breed = breed
+  # end
+  # 
+  # def breed
+  #   @breed
+  # end
 end
